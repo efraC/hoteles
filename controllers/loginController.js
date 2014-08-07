bf0b8e94e519385b095011ee10ed605e0a335697
Giovanny reyes ojeda <giovannyreyeso@gmail.com>
2014-08-07 17:11:10 -0600

    c
    c

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,22 +14,20 @@ exports.entrar = function(req,res,next)
  	var usuario_req = req.body.usuario;
  	var contrasena_req = req.body.contrasena;
 
-    usuarios.findOne({'usuario': usuario_req },function(err, usuario)
+  	usuarios.findOne({'usuario': usuario_req },function(err, usuario)
     {
-		if(err)
+		if(err) //ERROR DE CONEXION O ALGO ASI..
 		{
 			res.send(500, err.message);
 		}
-		if(usuario_req == usuario.usuario && contrasena_req == usuario.contrasena)
+		if(usuario && usuario_req == usuario.usuario && contrasena_req == usuario.contrasena)
 		{
-			return res.send("Usuario CORRECTO");
-		}else
+			  res.send("Usuario CORRECTO");
+		}
+		else
 		{
-			return res.json({ error : true });
+			res.json({ error : true });
 		}
     });
-    return res.json({ error : true });
-    console.log("hola :C");
-  
 };
 
