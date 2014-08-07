4b5b5fd53437287a5d2f68b43524e2237ecc83a7
Giovanny reyes ojeda <giovannyreyeso@gmail.com>
2014-08-07 11:28:35 -0600

    add model class
    asfa

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,6 @@
-/*var Schema = require('mongoose').Schema
+var Schema = require('mongoose').Schema
  
 var user_schema = new Schema({
   userName      :   String,
   userPassword  :   String
-})*/
+})
