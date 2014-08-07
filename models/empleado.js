2c982b3ed7423828eb2c713329d586caa86d19b0
Giovanny reyes ojeda <giovannyreyeso@gmail.com>
2014-08-07 11:51:35 -0600

    model class

---
+++ models/empleado.js
@@ -0,0 +1,8 @@
+/*var Schema = require('mongoose').Schema
+ 
+var empleado_schema = new Schema({
+  nombre     	:   String,
+  fechaNacimiento    :   String,
+  telefono    :   String,
+  direccion    :   String
+})*/
